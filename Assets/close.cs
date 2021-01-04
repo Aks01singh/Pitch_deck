c1b5d53b2d78984c1f01401165c9fb470a8bdc5d
Aks01singh <66474067+Aks01singh@users.noreply.github.com>
2021-01-04 12:19:38 +0530

    first

---
+++ Assets/close.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class close : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+       // if (Application.platform == RuntimePlatform.Android)
+        //{
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.Quit();
+            }
+       // }
+    }
+}
---
+++ Assets/tap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class tap : MonoBehaviour
+{
+    public GameObject gameObjectToInstantiate;
+    private GameObject spawnedObject;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    public GameObject arsessionorigin;
+    
+    [SerializeField] ARRaycast​Manager raycastManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+        Touch touch = Input.GetTouch(0);
+
+        if (raycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+                arsessionorigin.GetComponent<ARPlaneManager>().enabled = false;
+            }
+            // else
+            //    spawnedObject.transform.position = hitPose.position;
+
+        }
+    }
+}
